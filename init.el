1cf5262cc2db6ed2628e7ba86c54cfe77ac0b3ff
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2015-11-29 18:46:37 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path "~/.emacs.d/settings/")
+
+(require 'package-settings)
---
+++ settings/package-settings.el
@@ -0,0 +1,10 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa"."http://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+(package-initialize)
+
+(provide 'package-settings)
