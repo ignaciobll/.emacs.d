c2d4f16d3fb682a52a0e0ab5a8c28939eb658c5e
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2015-11-29 20:02:53 +0100

    Creado /themes. Añadido cyber-punk theme

--- settings/package-settings.el
+++ settings/package-settings.el
@@ -1,10 +1,10 @@
 (require 'package)
-(add-to-list 'package-archives
+  (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
+  (add-to-list 'package-archives
              '("melpa"."http://melpa.org/packages/"))
-(add-to-list 'package-archives
+  (add-to-list 'package-archives
              '("elpy" . "http://jorgenschaefer.github.io/packages/"))
-(package-initialize)
+  (package-initialize)
 
 (provide 'package-settings)
