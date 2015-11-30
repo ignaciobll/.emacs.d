7dbde56559d64d0aba645c1a5494806778e2d4e2
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2015-11-30 20:50:47 +0100

    Org Mode configured. Added global keybindings

--- init.el
+++ init.el
@@ -6,5 +6,19 @@
    t)
   (package-initialize))
 
+(add-to-list 'load-path "~/.emacs.d/modes/org-mode/lisp")
+(add-to-list 'load-path "~/.emacs.d/modes/org-mode/contrib/lisp" t)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
 (load-theme 'monokai t)
+
+
+;;----------------------------------------------------------------------
+;;-----  Global KeyBindings
+;;----------------------------------------------------------------------
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
