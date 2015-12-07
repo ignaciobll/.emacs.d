ff0c092c0e1221ce3c7c78f4f655fef0de2ffcc9
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2015-12-07 14:13:08 +0100

    Añadido Winner Mode

--- init.el
+++ init.el
@@ -6,13 +6,30 @@
    t)
   (package-initialize))
 
-(add-to-list 'load-path "~/.emacs.d/modes/org-mode/lisp")
-(add-to-list 'load-path "~/.emacs.d/modes/org-mode/contrib/lisp" t)
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(setq load-path (cons  "~/.emacs.d/modes/org-mode/lisp" load-path))
+(setq load-path (cons "~/.emacs.d/modes/org-mode/contrib/lisp" load-path))
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
+
+;;-------  Winner Moder --------------
+;; Mejor navegación entre ventanas
+
+(when (fboundp 'winner-mode)
+  (winner-mode 1))
+
+;;------------------------------------
+
 (load-theme 'monokai t)
 
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+
+(require 'multi-term)
+
+(setq multi-term-program "/bin/zsh")
 
 ;;----------------------------------------------------------------------
 ;;-----  Global KeyBindings
