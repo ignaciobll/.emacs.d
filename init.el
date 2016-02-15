6e75cd4b34e7574f7ae2677e80b2ef2b0a503a2d
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-02-15 09:36:33 +0100

    Añadido keybingind - magit popup

--- init.el
+++ init.el
@@ -117,7 +117,7 @@
 ;; ---------------------  MAGIT --------------------------------------
 
 (global-set-key (kbd "C-x g") 'magit-status)
-
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 
 
