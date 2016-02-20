b9045656c7c7f0a7201672bcd6daa3491713df80
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-02-20 17:33:01 +0100

    Auto indentado. Minted - exportado a color

--- init.el
+++ init.el
@@ -81,6 +81,24 @@
 
 (setq org-agenda-include-diary t)
 
+
+;;------------------ Minted -- colourful code exports -----------------
+
+
+
+;; Include the latex-exporter
+(require 'ox-latex)
+;; Add minted to the defaults packages to include when exporting.
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+;; Tell the latex export to use the minted package for source
+;; code coloration.
+(setq org-latex-listings 'minted)
+;; Let the exporter use the -shell-escape option to let latex
+;; execute external programs.
+;; This obviously and can be dangerous to activate!
+(setq org-latex-pdf-process
+      '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
 ;;----------------- Org Babel -----------------------------------------
 
 (org-babel-do-load-languages
@@ -91,6 +109,21 @@
    (sh . t)
    (python . t)
    )) ; this line activates dot
+
+;; ----------------- Auto indentado -----------------------------------
+
+;; Indent Fucking Whole Buffer
+(defun iwb ()
+  "Indent whole buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max))
+  (message "Indent buffer: Done.")
+  )
+
+(global-set-key "\M-i" 'iwb)
+
 
 ;;---------------- Ox - Reveal ----------------------------------------
 
