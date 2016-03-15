2f9662c28875f01167e238b8db083cd6a9db6254
Ignacio Balleteros <ignacioballesterosgonzalez@gmail.com>
2016-03-16 00:41:01 +0100

    More changes

--- elpa/dash-20151021.113/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash.el") (22111 3300 793003 768000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
